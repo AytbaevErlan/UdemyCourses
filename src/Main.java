67ab12fe82ef426301c37410cfa33f496a7537f4
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-02-07 20:41:37 +0100

    Basics

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
+        char myCharValue = 68;
+        char myCharValue2 = 63;
+        System.out.println(myCharValue + " " + myCharValue2);
         System.out.println("Hello world!");
     }
 }
