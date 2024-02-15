26a1257c6a8e09c260bff6d60794aef0b0a4e8f7
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-02-15 17:30:29 +0100

    challenges

---
+++ src/udemyCourseAlgoDsa/LinkedList/Main.java
@@ -0,0 +1,12 @@
+package udemyCourseAlgoDsa.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedListOne = new LinkedList(4);
+
+        myLinkedListOne.printAll();
+    }
+
+}
