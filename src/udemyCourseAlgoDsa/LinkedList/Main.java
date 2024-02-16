9365bfdb40237f5227396bc6907001791c823bf2
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-02-16 02:27:57 +0100

    challenges

--- src/udemyCourseAlgoDsa/LinkedList/Main.java
+++ src/udemyCourseAlgoDsa/LinkedList/Main.java
@@ -13,6 +13,12 @@ public class Main {
 
 
         myLinkedList.printAll();
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeLast());
     }
 
 }
